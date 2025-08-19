9443a4891501282c5cb39813dbe455786fef7f83
AlekseiGreen <spacegoogls@gmail.com>
2025-08-19 23:56:02 +0300

    Add client

--- client.js
+++ client.js
@@ -1,3 +1,14 @@
-fetch('http://localhost:3000/api/comments')
-    .then(res => res.json())
-    .then(data => console.log(data));
+// fetch('http://localhost:3000/api/comments')
+//     .then(res => res.json())
+//     .then(data => console.log(data));
+
+
+new Promise(
+    (resolve, reject)=> {
+        setTimeout(()=> resolve("setTIMER"), 1000);
+    }
+).then((value2)=>{
+    console.log(value2);
+    console.log("RESOLVE");
+}
+);
